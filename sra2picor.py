d0b19507a6eaa4ac8f9d41c01b4b3a2a94747c56
DustinChen1986 <chendasong@gmail.com>
2020-12-04 11:46:20 +0800

    Update sra2picor.py

--- sra2picor.py
+++ sra2picor.py
@@ -1,45 +1,51 @@
 import os
-srafiles = ['SRR4254090', 'SRR4254091', 'SRR4254092']
+import argparse
+
+parser = argparse.ArgumentParser(description='SRA fishing picornavirus')
+parser.add_argument('-s', '--sra', type=str, help='SRA file, separate with comma')
+parser.add_argument('-f', '--folder', type=str, default='./', help='SRA folder')
+parser.add_argument('-t', '--threads', type=int, default=12, help='CPU threads')
+args = parser.parse_args()
+srafiles = args.sra.split(',')
+srafolder = args.folder
+threads = args.threads
+
 for sra in srafiles:
-    os.system('fastq-dump -O /media/sf_G/picornavirus/ --split-3 /media/sf_G/picornavirus/'+sra+'.sra')
+    os.system('fastq-dump --split-3 '+srafolder+sra)
 pairend = []
 single = []
-for root, dirs, files in os.walk("/media/sf_G/picornavirus/"):
-    for sra in srafiles:
-        if sra+'_2.fastq' in files:
-            pairend.append(sra)
-        else:
-            single.append(sra)
+files = os.listdir("./")
+for sra in srafiles:
+    if sra+'_2.fastq' in files:
+        pairend.append(sra)
+    else:
+        single.append(sra)
 pairend=list(set(pairend))
 single=list(set(single))
 def pairend_assemble(pairend):
     fastq1='-1 '
     fastq2='-2 '
     for i in pairend:
-        os.system('trimmomatic PE -threads 11 -phred33 /media/sf_G/picornavirus/' #trim
-        +i+'_1.fastq /media/sf_G/picornavirus/'+i+'_2.fastq /media/sf_G/picornavirus/'
-        +i+'_1_p.fastq /media/sf_G/picornavirus/'+i+'_1_u.fastq /media/sf_G/picornavirus/'+i+'_2_p.fastq /media/sf_G/picornavirus/'+i+'_2_u.fastq '
+        os.system('trimmomatic PE -threads '+threads+' -phred33 ' #trim
+        +i+'_1.fastq '+i+'_2.fastq '
+        +i+'_1_p.fastq '+i+'_1_u.fastq '+i+'_2_p.fastq '+i+'_2_u.fastq '
         +'ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36')
-        os.system('rm /media/sf_G/picornavirus/'+i+'_1.fastq /media/sf_G/picornavirus/'+i+'_2.fastq /media/sf_G/picornavirus/' #remove fastq
-        +i+'_1_u.fastq /media/sf_G/picornavirus/'+i+'_2_u.fastq')
-        fastq1 +='/media/sf_G/picornavirus/'+i+'_1_p.fastq,'
-        fastq2 +='/media/sf_G/picornavirus/'+i+'_2_p.fastq,'
-    os.system('megahit -t 11  -o megahit_out '+fastq1[:-1]+' '+fastq2[:-1])
-    os.system('mv megahit_out /media/sf_G/picornavirus/')
-    os.system('diamond blastx -p 11 -d /media/sf_G/picornavirus/cds.dmnd -q /media/sf_G/picornavirus/megahit_out/final.contigs.fa --outfmt 6 -o diamond.txt')
-    os.system('mv diamond.txt /media/sf_G/picornavirus/megahit_out/')
+        os.system('rm '+i+'_1.fastq '+i+'_2.fastq ' #remove fastq
+        +i+'_1_u.fastq '+i+'_2_u.fastq')
+        fastq1 += i+'_1_p.fastq,'
+        fastq2 += i+'_2_p.fastq,'
+    os.system('megahit -t '+threads+' -o megahit_out '+fastq1[:-1]+' '+fastq2[:-1])
+    os.system('diamond blastx -p '+threads+' -d cds.dmnd -q ./megahit_out/final.contigs.fa --outfmt 6 -o diamond.txt')
 def single_assemble(single):
     fastq = ''
     for i in single:
-        os.system('trimmomatic SE -threads 11 -phred33 /media/sf_G/picornavirus/'+i+'.fastq /media/sf_G/picornavirus/'+i+'_s.fastq '
+        os.system('trimmomatic SE -threads '+threads+' -phred33 '+i+'.fastq '+i+'_s.fastq '
         + 'ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36')
-        os.system('rm /media/sf_G/picornavirus/'+i+'.fastq')
-        fastq += '/media/sf_G/picornavirus/'+i+'_s.fastq,'
+        os.system('rm '+i+'.fastq')
+        fastq += i+'_s.fastq,'
     print(fastq)
-    os.system('megahit -t 11 -o megahit_out -r '+fastq[:-1])
-    os.system('mv megahit_out /media/sf_G/picornavirus/')
-    os.system('diamond blastx -p 11 -d /media/sf_G/picornavirus/cds.dmnd -q /media/sf_G/picornavirus/megahit_out/final.contigs.fa --outfmt 6 -o diamond.txt')
-    os.system('mv diamond.txt /media/sf_G/picornavirus/megahit_out/')
+    os.system('megahit -t '+threads+' -o megahit_out -r '+fastq[:-1])
+    os.system('diamond blastx -p '+threads+' -d cds.dmnd -q ./megahit_out/final.contigs.fa --outfmt 6 -o diamond.txt')
 if pairend:
     pairend_assemble(pairend)
 elif single:
